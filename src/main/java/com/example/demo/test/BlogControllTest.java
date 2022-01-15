2dfa1f6ab37cdb5bd2ecabbaab96385561307535
junyoung choi <xlsk376@gmail.com>
2022-01-15 14:52:46 +0900

    환경세팅완료

---
+++ src/main/java/com/example/demo/test/BlogControllTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllTest {
+
+		//http://localhost:8080/test/hello
+		@GetMapping("/test/hello")
+		public String hello() {
+			return "<h1>hello spring boot</h1>";
+		}
+
+}
+
+
